6c82092238ffa4cb7de4e61b64755e99b7e451b1
kien <kienvonguyen1202@gmail.com>
2023-04-27 01:53:49 +0700

    create new class

--- src/main/java/Service/AdminService.java
+++ src/main/java/Service/AdminService.java
@@ -2,8 +2,40 @@ package Service;
 
 import DataAccess.DataAccess;
 import Middleware.UserMiddleware;
+import Model.User.User;
 
-public class AdminService extends UserServices{
+import java.util.HashMap;
+
+public class AdminService implements Services<User>{
     private final DataAccess db = new DataAccess();
-    private final UserMiddleware checker = new UserMiddleware();
+
+    @Override
+    public String idCreation() {
+        return null;
+    }
+
+    @Override
+    public void add(User template) {
+
+    }
+
+    @Override
+    public void edit(User template) {
+
+    }
+
+    @Override
+    public void delete(User template) {
+
+    }
+
+    @Override
+    public User getOne(String id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, User> getAll() {
+        return null;
+    }
 }
