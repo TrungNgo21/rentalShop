77f4f992da71f1d94c16a59ea6906fc4cd70f882
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-04-27 02:05:48 +0700

    Create branch

--- src/main/java/Model/User/Admin.java
+++ src/main/java/Model/User/Admin.java
@@ -1,7 +1,14 @@
 package Model.User;
 
+import Model.Product.Product;
+import Service.ProductService;
+
+import java.util.HashMap;
+
 public class Admin extends User {
     public Admin(String userId, String userName, String password, String fullName, String address, String phoneNum, String imageLocation) {
         super(userId, userName, password, fullName, address, phoneNum, imageLocation);
     }
+
+
 }
