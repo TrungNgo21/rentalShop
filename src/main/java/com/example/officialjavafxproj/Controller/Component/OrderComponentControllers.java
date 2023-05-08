33ee85854b2920b60c08a89f80489d6131aa7862
TrungNgo21 <ngolambaotrung@gmail.com>
2023-05-08 20:35:33 +0700

    Fix bug promote ccount

--- src/main/java/com/example/officialjavafxproj/Controller/Component/OrderComponentControllers.java
+++ src/main/java/com/example/officialjavafxproj/Controller/Component/OrderComponentControllers.java
@@ -12,7 +12,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
