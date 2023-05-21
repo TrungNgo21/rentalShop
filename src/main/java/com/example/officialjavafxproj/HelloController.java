bd9983f27d763d2b4a9d922714f6712b3b6fd583
TrungNgo21 <ngolambaotrung@gmail.com>
2023-05-21 14:28:50 +0700

    style Welcome page and register page

--- src/main/java/com/example/officialjavafxproj/HelloController.java
+++ src/main/java/com/example/officialjavafxproj/HelloController.java
@@ -1,17 +1,25 @@
 package com.example.officialjavafxproj;
 
 import DataAccess.DataAccess;
+import FileLocation.FileLocation;
 import com.example.officialjavafxproj.Utils.SceneController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     private AnchorPane anchorPane;
@@ -28,6 +36,9 @@ public class HelloController {
     private Button exitButton;
 
     @FXML
+    private ImageView backgroundImg;
+
+    @FXML
     public void onLoginButton(ActionEvent event) throws IOException {
         SceneController sceneController = new SceneController();
         sceneController.switchScene(event, "../Pages/login.fxml");
@@ -42,5 +53,19 @@ public class HelloController {
         DataAccess.transferAllData();
         Stage stage = (Stage) anchorPane.getScene().getWindow();
         stage.close();
+    }
+
+    public void loadBackgroundImg(){
+        try {
+            Image img = new Image(new FileInputStream(new FileLocation().getImageDir() + "/Public/headerIcon.jpg"), 600, 400, false, false);
+            backgroundImg.setImage(img);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadBackgroundImg();
     }
 }
